d7e8f6a2fe52ef01aa8929d0db5b95457bbd5aee
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-12-15 01:24:45 -0500

    made the modal larger and gave it a black background so we can hide the welcome tags and button

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const modalBox = document.getElementById('Quiz-Modal-Slide');
+const openModal = document.querySelector('.open-modal');
 
 function openQuizSlide (){
     modalBox.style.display = "block";
+    openModal.style.display = "none";
 }
 function closeQuizSlide (){
     modalBox.style.display = "none";
+    openModal.style.display = "block";
 }
