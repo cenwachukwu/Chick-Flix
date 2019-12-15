51388b0f9ae488bb28884a3ea0c1d4aa277fdf86
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-12-14 23:28:59 -0500

    created the modal box holding quiz

---
+++ script.js
@@ -0,0 +1,8 @@
+const modalBox = document.getElementById('Quiz-Modal-Slide');
+
+function openQuizSlide (){
+    modalBox.style.display = "block";
+}
+function closeQuizSlide (){
+    modalBox.style.display = "none";
+}
