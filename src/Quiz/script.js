769f5084cad33147e79da3736861b54e69fa230b
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-12-19 23:24:49 -0500

    added events to the buttons

--- src/Quiz/script.js
+++ src/Quiz/script.js
@@ -1,15 +1,23 @@
 const modalBox = document.getElementById('Quiz-Modal-Slide');
 const openModal = document.querySelector('.open-modal');
 const Quiz = document.querySelectorAll(".Quiz");
+const option1 = document.querySelector(".option1")
+const option2 = document.querySelector(".option2")
+const option3 = document.querySelector(".option3")
+const option4 = document.querySelector(".option4")
+const next = document.querySelector(".next")
 
 function openQuizSlide() {
     modalBox.style.display = "block";
     openModal.style.display = "none";
 }
 
-//closing the modal
 function closeQuizSlide() {
     modalBox.style.display = "none";
     openModal.style.display = "block";
     location.reload()
+}
+
+function option() {
+    alert ("I have been Clicked!!!")
 }
