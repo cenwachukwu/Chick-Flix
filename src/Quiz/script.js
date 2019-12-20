5496a1e840e5fcb9950d0329b4364282108cf07e
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-12-20 09:14:59 -0500

    added an event listener to the quiz button to pull data from an api

--- src/Quiz/script.js
+++ src/Quiz/script.js
@@ -1,6 +1,7 @@
 const modalBox = document.getElementById('Quiz-Modal-Slide');
 const openModal = document.querySelector('.open-modal');
 const Quiz = document.querySelectorAll(".Quiz");
+const Question = document.querySelector(".Question");
 const option1 = document.querySelector(".option1")
 const option2 = document.querySelector(".option2")
 const option3 = document.querySelector(".option3")
@@ -20,3 +21,20 @@ function closeQuizSlide() {
 function option() {
     alert ("I have been Clicked!!!")
 }
+// setting evt to pull infomation from the api
+const url3 = "https://opentdb.com/api.php?amount=10&category=20&difficulty=easy&type=multiple"
+openModal.addEventListener("click", function (evt) {
+    evt.preventDefault()
+    console.log(evt)
+    fetch(url3)
+        .then(res => res.json())
+        .then(res => {
+            console.log(res.results[0])
+            Question.innerText = res.results[0].question
+            option1.innerText = res.results[0].incorrect_answers[2]
+            option2.innerText = res.results[0].correct_answer
+            option3.innerText = res.results[0].incorrect_answers[0]
+            option4.innerText = res.results[0].incorrect_answers[1]
+        })
+        .catch(err => console.log(err))
+}) 
