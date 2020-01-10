ef88d11823e3b88605effe8eee312c0da61afbcf
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-01-09 20:24:24 -0500

    refactoring the quiz

--- src/Quiz/script.js
+++ src/Quiz/script.js
@@ -1,12 +1,12 @@
 const modalBox = document.getElementById('Quiz-Modal-Slide');
 const openModal = document.querySelector('.open-modal');
 const Quiz = document.querySelectorAll(".Quiz");
+const category = document.querySelector(".category");
 const Question = document.querySelector(".Question");
-const option1 = document.querySelector(".option1")
-const option2 = document.querySelector(".option2")
-const option3 = document.querySelector(".option3")
-const option4 = document.querySelector(".option4")
-const next = document.querySelector(".next")
+const answer = document.querySelector(".answer");
+const next = document.querySelector(".next");
+const input = document.querySelector(".input");
+const submitInput = document.querySelector(".option1");
 
 function openQuizSlide() {
     modalBox.style.display = "block";
@@ -18,69 +18,34 @@ function closeQuizSlide() {
     openModal.style.display = "inline-block";
 }
 
-// a callback function to show that buttons work
-function option() {
-    alert("I have been Clicked!!!")
-}
+// data for the quiz
 
-// make an empty array to store data from the api
-let quizCards = []
-
-let i = 0
-
-function shuffle(quizCards) {
-    var m = quizCards.length, t, i;
-
-    // While there remain elements to shuffle…
-    while (m) {
-
-        // Pick a remaining element…
-        i = Math.floor(Math.random() * m--);
-
-        // And swap it with the current element.
-        t = quizCards[m];
-        quizCards[m] = quizCards[i];
-        quizCards[i] = t;
+const myQuestions = [
+    {
+      question: "who are you?",
+      answers: {
+        a: "nneoma",
+        b: "super man",
+        c: "speedometer"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "are you the best sister?",
+      answers: {
+        a: "site 1",
+        b: "numbers",
+        c: "shape"
+      },
+      correctAnswer: "site 1"
+    },
+    {
+      question: "How many friends i have?",
+      answers: {
+        a: "i love school",
+        b: "i love my sister",
+        c: "i love snow",
+      },
+      correctAnswer: "b"
     }
-
-    return quizCards;
-}
-
-function loadCard() {
-    Question.innerText = quizCards[i].question
-    option1.innerText = quizCards[i].incorrect_answers[2]
-    option2.innerText = quizCards[i].correct_answer
-    option3.innerText = quizCards[i].incorrect_answers[0]
-    option4.innerText = quizCards[i].incorrect_answers[1]
-};
-
-function nextItem() {
-    i = i + 1; // increase i by one
-    i = i % quizCards.length; // if we've gone too high, start from `0` again
-    return quizCards[i]; // give us back the item of where we are now
-}
-
-// setting evt to pull infomation from the api
-const url3 = "https://opentdb.com/api.php?amount=10&category=17"
-openModal.addEventListener("click", function (evt) {
-    evt.preventDefault()
-    console.log(evt)
-    fetch(url3)
-        .then(res => res.json())
-        .then(res => {
-            console.log(res.results)
-            let data = res.results
-            quizCards.push(...data);
-            console.log(quizCards)
-            shuffle(quizCards)
-            console.log(quizCards)
-            loadCard()
-        })
-        .catch(err => console.log(err))
-})
-
-next.addEventListener("click", function (evt) {
-    evt.preventDefault
-    nextItem()
-    loadCard()
-})
+];
